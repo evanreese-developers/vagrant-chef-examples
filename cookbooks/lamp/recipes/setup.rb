5efbf4338d7cffd40fe0178015abdba80ec32c71
Evan Reese <development@evanreese.com>
2019-10-24 23:48:23 -0400

    i[dating setup

--- cookbooks/lamp/recipes/setup.rb
+++ cookbooks/lamp/recipes/setup.rb
@@ -3,3 +3,9 @@
         action :install
     end
 end
+execute "epel" do
+    commannd 'rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm'
+end
+execute "webtatic" do
+    rpm -Uvh 'https://mirror.webtatic.com/yum/el7/webtatic-release.rpm'
+end
