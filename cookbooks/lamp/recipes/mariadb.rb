fdfdb08f103ae8946cdb037e516fdb179b26c5ae
Evan Reese <development@evanreese.com>
2019-10-28 23:53:25 -0400

    adding comments

--- cookbooks/lamp/recipes/mariadb.rb
+++ cookbooks/lamp/recipes/mariadb.rb
@@ -1,3 +1,4 @@
+#note we are using the mysql password even though this is mariadb as it is being used in LAMP Stack
 mariadb_config = search(:passwords, "id:mysql").first
 bash 'configure mariadb' do
     code <<- EOT
